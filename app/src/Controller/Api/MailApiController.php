99f805d077b6cb47759c4c8bdcf9c15d364ba86f
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-03 18:39:27 -0800

    Add create mail action

--- app/src/Controller/Api/MailApiController.php
+++ app/src/Controller/Api/MailApiController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Api;
 
+use App\Model\Mail\Api\Creator\MailCreatorInterface;
 use App\Model\Mail\Api\Reader\MailReaderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,6 +28,29 @@ class MailApiController extends BaseApiController
         return $this->createApiResponse(
             $mailReader->findPaginated($request->query->getInt(static::KEY_PAGE, 1)),
             Response::HTTP_OK,
+        );
+    }
+
+    /**
+     * @Route("/api/mails", methods={"POST"})
+     *
+     * @param Request $request
+     * @param MailCreatorInterface $mailCreator
+     *
+     * @return Response
+     */
+    public function create(
+        Request $request,
+        MailCreatorInterface $mailCreator
+    ): Response {
+        $mail = $mailCreator->createFromType(
+            $request->getContent(),
+            $request->getContentType() ?? 'json'
+        );
+
+        return $this->createApiResponse(
+            $mail,
+            Response::HTTP_CREATED,
         );
     }
 }
