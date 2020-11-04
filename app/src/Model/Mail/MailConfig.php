3689903f7f1571ffb37aabd2794825a46e3deeef
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-03 21:07:50 -0800

    Allow to use and extend config consts

--- app/src/Model/Mail/MailConfig.php
+++ app/src/Model/Mail/MailConfig.php
@@ -5,13 +5,13 @@ namespace App\Model\Mail;
 
 class MailConfig
 {
-    private const PAGE_SIZE = 10;
+    protected const PAGE_SIZE = 10;
 
-    private const CONTENT_TYPE_HTML = 'text/html';
-    private const CONTENT_TYPE_TEXT = 'text/plain';
-    private const CONTENT_TYPE_MARKDOWN = 'text/markdown';
+    public const CONTENT_TYPE_HTML = 'text/html';
+    public const CONTENT_TYPE_TEXT = 'text/plain';
+    public const CONTENT_TYPE_MARKDOWN = 'text/markdown';
 
-    private const ALLOWED_CONTENT_TYPES = [
+    protected const ALLOWED_CONTENT_TYPES = [
         self::CONTENT_TYPE_HTML,
         self::CONTENT_TYPE_TEXT,
         self::CONTENT_TYPE_MARKDOWN,
