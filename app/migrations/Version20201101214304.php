a465379169e58e1c07041805f9b4aa42f5060125
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-03 18:34:19 -0800

    Remove not needed index

--- app/migrations/Version20201101214304.php
+++ app/migrations/Version20201101214304.php
@@ -21,7 +21,7 @@ final class Version20201101214304 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE content (id INT AUTO_INCREMENT NOT NULL, mail_id INT DEFAULT NULL, type VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, INDEX IDX_FEC530A9C8776F01 (mail_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE mail (id INT AUTO_INCREMENT NOT NULL, recipient_id INT NOT NULL, subject VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, sent_at DATETIME, UNIQUE INDEX UNIQ_5126AC48FBCE3E7A (subject), UNIQUE INDEX UNIQ_5126AC48E92F8F78 (recipient_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE mail (id INT AUTO_INCREMENT NOT NULL, recipient_id INT NOT NULL, subject VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, sent_at DATETIME, UNIQUE INDEX UNIQ_5126AC48E92F8F78 (recipient_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE recipient (id INT AUTO_INCREMENT NOT NULL, mail_id INT DEFAULT NULL, email VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_6804FB49C8776F01 (mail_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE content ADD CONSTRAINT FK_FEC530A9C8776F01 FOREIGN KEY (mail_id) REFERENCES mail (id)');
         $this->addSql('ALTER TABLE mail ADD CONSTRAINT FK_5126AC48E92F8F78 FOREIGN KEY (recipient_id) REFERENCES recipient (id)');
