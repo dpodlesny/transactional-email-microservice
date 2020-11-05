6ba6d2169957e13140255fb1d8a0c6c9aa91bd4f
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-04 20:34:26 -0800

    Implement Mail Saver

---
+++ app/src/Model/Mail/Saver/MailSaver.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Mail\Saver;
+
+use App\Entity\Mail;
+use App\Model\Mail\Validator\MailValidatorInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class MailSaver implements MailSaverInterface
+{
+    /**
+     * @var MailValidatorInterface
+     */
+    protected MailValidatorInterface $validator;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected EntityManagerInterface $em;
+
+    /**
+     * @param MailValidatorInterface $validator
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(
+        MailValidatorInterface $validator,
+        EntityManagerInterface $em
+    ) {
+        $this->validator = $validator;
+        $this->em = $em;
+    }
+
+    /**
+     * @param Mail $mail
+     *
+     * @return Mail
+     */
+    public function save(Mail $mail): Mail
+    {
+        if ($this->validator->validate($mail) === false) {
+            throw new BadRequestHttpException($this->validator->getErrorMessage());
+        }
+
+        $this->em->persist($mail->getRecipient());
+        $this->em->persist($mail);
+        $this->em->flush();
+        $this->em->refresh($mail);
+
+        return $mail;
+    }
+}
---
+++ app/src/Model/Mail/Saver/MailSaverInterface.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Mail\Saver;
+
+use App\Entity\Mail;
+
+interface MailSaverInterface
+{
+    /**
+     * Specification:
+     *  - Validates a Mail entity.
+     *  - Throws an exception if Mail entity is invalid.
+     *  - Stores a Mail entity into the DB.
+     *
+     * @param Mail $mail
+     *
+     * @return Mail
+     */
+    public function save(
+        Mail $mail
+    ): Mail;
+}
