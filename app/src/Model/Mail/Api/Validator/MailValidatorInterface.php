4c6fcb63de128e37ef7790c999e29afd9dea6af6
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-04 18:50:41 -0800

    Add specification to MailValidatorInterface

--- app/src/Model/Mail/Api/Validator/MailValidatorInterface.php
+++ app/src/Model/Mail/Api/Validator/MailValidatorInterface.php
@@ -8,6 +8,10 @@ use App\Entity\Mail;
 interface MailValidatorInterface
 {
     /**
+     * Specification:
+     *  - Validates Mail object.
+     *  - Returns true if object is valid otherwise false.
+     *
      * @param Mail $mail
      *
      * @return bool
@@ -15,6 +19,9 @@ interface MailValidatorInterface
     public function validate(Mail $mail): bool;
 
     /**
+     * Specification:
+     *  - Returns string with error messages if object is invalid.
+     *
      * @return string|null
      */
     public function getErrorMessage(): ?string;
