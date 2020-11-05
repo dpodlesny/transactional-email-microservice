9d443ed0279528e31fbf781b011ee1825f4da6a7
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-04 18:50:26 -0800

    Add specification to MailCreatorInterface

--- app/src/Model/Mail/Api/Creator/MailCreatorInterface.php
+++ app/src/Model/Mail/Api/Creator/MailCreatorInterface.php
@@ -8,6 +8,13 @@ use App\Entity\Mail;
 interface MailCreatorInterface
 {
     /**
+     * Specification:
+     *  - Creates Mail entity from a data string (e.x. "json").
+     *  - Validates a Mail entity.
+     *  - Throws an exception if Mail entity is invalid.
+     *  - Stores a Mail entity into the DB.
+     *  - Pushes message with Mail entity to queue.
+     *
      * @param string $mailData
      * @param string $type
      *
