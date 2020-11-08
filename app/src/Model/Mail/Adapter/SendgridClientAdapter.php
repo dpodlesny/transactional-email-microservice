83cfa082fe94a02dc2055ab695ae0745fb69fa2f
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-07 19:57:26 -0800

    Implement SendgridClientAdapter

---
+++ app/src/Model/Mail/Adapter/SendgridClientAdapter.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Mail\Adapter;
+
+use App\Entity\Mail;
+use App\Model\Mail\MailConfig;
+use Exception;
+use SendGrid;
+use SendGrid\Mail\Mail as SendGridMail;
+
+class SendgridClientAdapter implements MailClientAdapterInterface
+{
+    /**
+     * @var SendGrid
+     */
+    protected SendGrid $sendGridClient;
+
+    /**
+     * @param string $sendGridApiKey
+     */
+    public function __construct(string $sendGridApiKey)
+    {
+        $this->sendGridClient = new SendGrid($sendGridApiKey);
+    }
+
+    /**
+     * @param Mail $mail
+     *
+     * @return bool
+     */
+    public function send(Mail $mail): bool
+    {
+        $email = new SendGridMail();
+        $email->setFrom(MailConfig::getFromEmail(), MailConfig::getFromName());
+        $email->setSubject($mail->getSubject());
+        $email->addTo($mail->getRecipient()->getEmail(), $mail->getRecipient()->getName());
+
+        foreach ($mail->getContents() as $content) {
+            $email->addContent($content->getType(), $content->getContent());
+        }
+
+        foreach ($mail->getAdditionalRecipients() as $additionalRecipient) {
+            $email->addCc($additionalRecipient->getEmail(), $additionalRecipient->getName());
+        }
+
+        try {
+            $response = $this->sendGridClient->send($email);
+
+            return $response->statusCode() >= 200 && $response->statusCode() <= 299;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+}
