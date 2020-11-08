297e26c8cef009057ecdcb0f01ce7b5035cf5bbf
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-07 20:15:58 -0800

    Implement QueuePublisherInterface

---
+++ app/src/Model/Queue/Publisher/QueuePublisherInterface.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Queue\Publisher;
+
+interface QueuePublisherInterface
+{
+    /**
+     * @param string $queueName
+     * @param string $exchangeName
+     * @param string $messageBody
+     *
+     * @return void
+     */
+    public function publish(
+        string $queueName,
+        string $exchangeName,
+        string $messageBody
+    ): void;
+}
