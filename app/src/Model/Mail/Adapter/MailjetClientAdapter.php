6028499b6bc0c56eaa1920252ced8c12e4e08534
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-07 19:56:51 -0800

    Implement MailjetClientAdapter

---
+++ app/src/Model/Mail/Adapter/MailjetClientAdapter.php
@@ -0,0 +1,86 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Mail\Adapter;
+
+use App\Entity\Mail;
+use App\Model\Mail\MailConfig;
+use Mailjet\Client;
+use Mailjet\Resources;
+
+class MailjetClientAdapter implements MailClientAdapterInterface
+{
+    /**
+     * @var Client
+     */
+    protected Client $mailjetClient;
+
+    public function __construct(
+        string $mailjetApiKeyPublic,
+        string $mailjetApiKeyPrivate
+    ) {
+        $this->mailjetClient = new Client(
+            $mailjetApiKeyPublic,
+            $mailjetApiKeyPrivate,
+            true,
+            ['version' => 'v3.1']
+        );
+    }
+
+    /**
+     * @param Mail $mail
+     *
+     * @return bool
+     */
+    public function send(Mail $mail): bool
+    {
+        $htmlPart = '';
+        $textPart = '';
+
+        foreach ($mail->getContents() as $content) {
+            if ($content->getType() === MailConfig::CONTENT_TYPE_HTML) {
+                $htmlPart = $content->getContent();
+                continue;
+            }
+
+            if ($content->getType() === MailConfig::CONTENT_TYPE_TEXT) {
+                $textPart = $content->getContent();
+                continue;
+            }
+        }
+
+        $cc = [];
+
+        foreach ($mail->getAdditionalRecipients() as $additionalRecipient) {
+            $cc[] = [
+                'Email' => $additionalRecipient->getEmail(),
+                'Name' => $additionalRecipient->getName(),
+            ];
+        }
+
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => MailConfig::getFromEmail(),
+                        'Name' => MailConfig::getFromName(),
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $mail->getRecipient()->getEmail(),
+                            'Name' => $mail->getRecipient()->getName(),
+                        ],
+                    ],
+                    'CC' => $cc,
+                    'Subject' => $mail->getSubject(),
+                    'TextPart' => $textPart,
+                    'HTMLPart' => $htmlPart,
+                ],
+            ],
+        ];
+
+        $response = $this->mailjetClient->post(Resources::$Email, ['body' => $body]);
+
+        return $response->success();
+    }
+}
