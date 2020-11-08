b8ffdb39984a442bbdb3e0fb4af27aacb222986d
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-07 19:50:40 -0800

    Introduce MailClientAdapterInterface

---
+++ app/src/Model/Mail/Adapter/MailClientAdapterInterface.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Mail\Adapter;
+
+use App\Entity\Mail;
+
+interface MailClientAdapterInterface
+{
+    /**
+     * Specification:
+     *  - Sends email with mail service.
+     *  - Returns true if mail was sent otherwise false.
+     *
+     * @param Mail $mail
+     *
+     * @return bool
+     */
+    public function send(Mail $mail): bool;
+}
