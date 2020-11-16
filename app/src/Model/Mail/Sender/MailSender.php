eb3ef3a01b4e60c36cf53cb4f4c5322f22c2a081
Denys Podliesnyi <underwood.dv@gmail.com>
2020-11-16 09:37:18 -0800

    Add circuit breaker usage

--- app/src/Model/Mail/Sender/MailSender.php
+++ app/src/Model/Mail/Sender/MailSender.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace App\Model\Mail\Sender;
 
 use App\Entity\Mail;
+use App\Model\CircuitBreaker\Breaker\MailCircuitBreakerInterface;
+use App\Model\CircuitBreaker\Exception\MailServicesNotAvailableException;
 use App\Model\Mail\Adapter\MailClientAdapterInterface;
+use Psr\Log\LoggerInterface;
 
 class MailSender implements MailSenderInterface
 {
@@ -20,15 +23,31 @@ class MailSender implements MailSenderInterface
     protected array $fallbackMailClients;
 
     /**
+     * @var MailCircuitBreakerInterface
+     */
+    protected MailCircuitBreakerInterface $mailCircuitBreaker;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected LoggerInterface $logger;
+
+    /**
      * @param MailClientAdapterInterface $mainMailClient
      * @param array<MailClientAdapterInterface> $fallbackMailClients
+     * @param MailCircuitBreakerInterface $mailCircuitBreaker
+     * @param LoggerInterface $logger
      */
     public function __construct(
         MailClientAdapterInterface $mainMailClient,
-        array $fallbackMailClients
+        array $fallbackMailClients,
+        MailCircuitBreakerInterface $mailCircuitBreaker,
+        LoggerInterface $logger
     ) {
         $this->mainMailClient = $mainMailClient;
         $this->fallbackMailClients = $fallbackMailClients;
+        $this->mailCircuitBreaker = $mailCircuitBreaker;
+        $this->logger = $logger;
     }
 
     /**
@@ -38,14 +57,31 @@ class MailSender implements MailSenderInterface
      */
     public function send(Mail $mail): bool
     {
-        if ($this->mainMailClient->send($mail)) {
+        $isSent = $this->mailCircuitBreaker
+            ->setMailClientAdapter($this->mainMailClient)
+            ->handle($mail);
+
+        if ($isSent === true) {
             return true;
         }
 
         foreach ($this->fallbackMailClients as $mailService) {
-            if ($mailService->send($mail)) {
+            $isSent = $this->mailCircuitBreaker
+                ->setMailClientAdapter($mailService)
+                ->handle($mail);
+
+            if ($isSent) {
+                $fallBackClass = get_class($mailService);
+                $this->logger->warning("Mail#{$mail->getId()} was sent via fallback mail service: {$fallBackClass}");
+
                 return true;
             }
+        }
+
+        if ($isSent === null) {
+            $this->logger->critical('Mail services is not responding.');
+
+            throw new MailServicesNotAvailableException();
         }
 
         return false;
